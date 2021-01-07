56af997bd2ba70a0637867e7546fbe71695c231c
amitmahajan <amitmahajan@post.com>
2021-01-07 21:03:25 +0000

    updated controller

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -14,6 +14,7 @@ public class DemoController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println("enter");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }

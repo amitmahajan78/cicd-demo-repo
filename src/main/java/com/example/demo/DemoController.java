7fe2ab4c7205bb5b1b5f089519f721fa6542a6d8
amitmahajan <amitmahajan@post.com>
2021-01-09 16:45:29 +0000

    added new functionality for release 11

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class DemoController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, [Release:11] - %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
